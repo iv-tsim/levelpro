7f966a0cf964d390352027a824b663489e255ad6
Ivan <iv-tsim@yandex.ru>
2021-02-16 13:29:00 +0300

    inst

--- js/main.js
+++ js/main.js
@@ -50,6 +50,31 @@
 
         });
 
+        let instSlider = new Swiper('.inst-slider', {
+
+            speed: 600,
+            spaceBetween: 40,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            observer: true,
+            observeParents: true,
+            touchEventsTarget: 'wrapper',
+            navigation: {
+                prevEl: '.slider-arrow.slider-arrow__prev.inst-arrow',
+                nextEl: '.slider-arrow.slider-arrow__next.inst-arrow'
+            },
+            pagination: {
+                el: '.slider-count',
+                type: 'custom',
+                renderCustom: function (swiper, current, total) {
+                    return '<div class="slider-count__current">' + addZero(current) + '</div>' +
+                            '<div class="slider-count__dec"></div>' +
+                            '<div class="slider-count__total">' + addZero(total) + '</div>';
+                }
+            }
+
+        });
+
         let partnersSlider = new Swiper('.partners-slider', {
 
             speed: 600,
