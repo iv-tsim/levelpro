37a35e0a69fc3df8cbe3731a8e730d369b9fc3aa
Ivan <iv-tsim@yandex.ru>
2021-02-17 14:05:42 +0300

    card

--- js/main.js
+++ js/main.js
@@ -120,6 +120,75 @@
 
         });
 
+        function updateCardSliderHeight() {
+
+            let cardSlider = document.querySelector('.card-slider'),
+                cardSliderWidth = cardSlider.clientWidth,
+                cardSliderHeight = cardSliderWidth * 1.38;
+
+                if (cardSlider) {
+                    cardSlider.style.height = cardSliderHeight + 'px';
+                }
+
+            return cardSliderHeight;
+                
+        }
+
+        let cardSlider = new Swiper('.card-slider', {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            pagination: {
+                el: '.card-slider__dots',
+                type: 'bullets'
+            },
+            mousewheel: true,
+            breakpoints: {
+                950: {
+                    direction: 'vertical',
+                    height: updateCardSliderHeight(),
+                },
+                0: {
+                    direction: 'horizontal',
+                    autoHeight: true
+                }
+            }
+        });
+
+        const cardSliders = document.querySelectorAll('.cardSlider');
+
+        cardSliders.forEach(function(item, index) {
+
+            let name = 'cardSlider_' + index;
+
+            item.classList.add(name);
+
+            item.closest('.swiper-container').querySelectorAll('.slider-arrow').forEach(function(item) {item.classList.add('slider-arrow_' + index)});
+
+            let popularSlider = new Swiper('.' + name, {
+                speed: 600,
+                spaceBetween: 40,
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+                observer: true,
+                observeParents: true,
+                touchEventsTarget: 'wrapper',
+                navigation: {
+                    prevEl: '.slider-arrow.slider-arrow__prev.card-arrow.slider-arrow_' + index,
+                    nextEl: '.slider-arrow.slider-arrow__next.card-arrow.slider-arrow_' + index
+                },
+                pagination: {
+                    el: '.slider-count',
+                    type: 'custom',
+                    renderCustom: function (swiper, current, total) {
+                        return '<div class="slider-count__current">' + addZero(current) + '</div>' +
+                                '<div class="slider-count__dec"></div>' +
+                                '<div class="slider-count__total">' + addZero(total) + '</div>';
+                    }
+                }
+            });
+
+        }); 
+
         document.addEventListener('click', function(event) {
 
             const { target } = event;
@@ -140,6 +209,14 @@
                     body.style.maxHeight = body.scrollHeight + 'px';
 
                 }
+
+            }
+
+            if (target.matches('.card-specs__item-value__img')) {
+
+                let text = target.closest('.card-specs__item-value_copy').querySelector('span').textContent;
+
+                navigator.clipboard.writeText(text);
 
             }
             
